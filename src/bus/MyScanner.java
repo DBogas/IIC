d032bd0f9ad4d68a4ca90e5af3190b921455ec58
Yisgramiir <dbogas738@gmail.com>
2016-10-29 23:55:05 +0100

    now all the stops get GPS coordinates.

--- src/bus/MyScanner.java
+++ src/bus/MyScanner.java
@@ -15,9 +15,16 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jdk.nashorn.internal.parser.JSONParser;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import com.google.gson.Gson;
 
 class MyScanner {
 
@@ -92,82 +99,123 @@ class MyScanner {
 
 		return result;
 	}
+
 	/**
-	 * method that iterates through all the lines
-	 * in each line it iterates through all the bus stops
-	 * if a bus stop isn't stored in the hashmap, stores it
+	 * method that iterates through all the lines in each line it iterates
+	 * through all the bus stops if a bus stop isn't stored in the hashmap,
+	 * stores it
 	 * 
-	 *  it creates a file for output too.
+	 * it creates a file for output too.
+	 * 
 	 * @throws Exception
 	 */
-	
-	static void getAllStops() throws Exception{
-		//file handling
+
+	static void getAllStops() throws Exception {
+		// file handling
 		File outputFile = new File("AllStops.txt");
-		//outputFile.createNewFile();
-		//actual code
+		// outputFile.createNewFile();
+		// actual code
 		HashMap<String, Stop> AllStops = new HashMap<String, Stop>();
 		String readFromURL = readUrl("http://www.stcp.pt/pt/itinerarium/callservice.php?action=lineslist&service=1&madrugada=1");
 		List<String> allLineNumbers = getAllLineNumbers(readFromURL);
-		
+
 		// iterate all the lines
-		for(String s : allLineNumbers){
-			
-			//we have to do this twice, for direction 0
-			List<JSONObject> aux = getLine(s,"0");
-			for(JSONObject o : aux){
-				Stop stop = new Stop(o.get("code").toString(),o.get("address").toString(),o.get("zone").toString(),o.get("name").toString());
-				if(!AllStops.containsKey(stop.code)) AllStops.put(stop.code, stop);
+		for (String s : allLineNumbers) {
+
+			// we have to do this twice, for direction 0
+			List<JSONObject> aux = getLine(s, "0");
+			for (JSONObject o : aux) {
+				float[] coords = getStopGPSCoords(o.get("code").toString());
+				Stop stop = new Stop(o.get("code").toString(), o.get("address")
+						.toString(), o.get("zone").toString(), o.get("name")
+						.toString());
+				stop.longitude = coords[0];
+				stop.latitude = coords[1];
+				if (!AllStops.containsKey(stop.code))
+					AllStops.put(stop.code, stop);
 			}
 			// and for direction 1
-			aux = getLine(s,"1");
-			for(JSONObject o : aux){
-				Stop stop = new Stop(o.get("code").toString(),o.get("address").toString(),o.get("zone").toString(),o.get("name").toString());
-				if(!AllStops.containsKey(stop.code)) AllStops.put(stop.code, stop);
+			aux = getLine(s, "1");
+			for (JSONObject o : aux) {
+				float[] coords = getStopGPSCoords(o.get("code").toString());
+				Stop stop = new Stop(o.get("code").toString(), o.get("address")
+						.toString(), o.get("zone").toString(), o.get("name")
+						.toString());
+				stop.longitude = coords[0];
+				stop.latitude = coords[1];
+				if (!AllStops.containsKey(stop.code))
+					AllStops.put(stop.code, stop);
 			}
-			// test print
-			for(String auxS : AllStops.keySet()){
+			// print
+			for (String auxS : AllStops.keySet()) {
 				AllStops.get(auxS).printStop();
 			}
-			
+
 		}
 		System.out.println(AllStops.size());
 	}
+
 	/**
-	 * this method aims to create a .txt file with all the Bus Lines in the following format
-	 * line code, direction
-	 * stop1_code, stop2_code, etc
-	 * @throws Exception 
+	 * this method aims to create a .txt file with all the Bus Lines in the
+	 * following format line code, direction stop1_code, stop2_code, etc
+	 * 
+	 * @throws Exception
 	 */
-	static void getAllLines() throws Exception{
-		//file handling
+	static void getAllLines() throws Exception {
+		// file handling
 		File outputFile = new File("AllLines.txt");
-		//actual code
+		// actual code
 		String readFromURL = readUrl("http://www.stcp.pt/pt/itinerarium/callservice.php?action=lineslist&service=1&madrugada=1");
 		List<String> allLineNumbers = getAllLineNumbers(readFromURL);
 		// iterating
-		for(String s : allLineNumbers){
-			List<JSONObject> aux1 = getLine(s,"0");
-			List<JSONObject> aux2 = getLine(s,"1");
+		for (String s : allLineNumbers) {
+			List<JSONObject> aux1 = getLine(s, "0");
+			List<JSONObject> aux2 = getLine(s, "1");
 			// print in direction 0
-			System.out.println(s+",0");
-			for(JSONObject o : aux1){
-				System.out.print(o.get("code")+" ");
+			System.out.println(s + ",0");
+			for (JSONObject o : aux1) {
+				System.out.print(o.get("code") + " ");
 			}
 			System.out.println();
 			// print in direction 1
-			System.out.println(s+",1");
-			for(JSONObject o : aux2){
-				System.out.print(o.get("code")+" ");
+			System.out.println(s + ",1");
+			for (JSONObject o : aux2) {
+				System.out.print(o.get("code") + " ");
 			}
 			System.out.println();
 		}
 	}
-	
+
+	static float[] getStopGPSCoords(String stopCode) throws Exception {
+		String readFromURL = readUrl("http://www.stcp.pt/pt/itinerarium/callservice.php?action=srchstoplines&stopcode=BLRB1");
+		JSONArray ja = new JSONArray(readFromURL);
+		JSONObject jo = (JSONObject) ja.get(0);
+		String s = (String) jo.get("geomdesc");
+		// saturday night delight parsing
+		String aux = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '-')
+				aux += '-';
+			if (s.charAt(i) == '.')
+				aux += '.';
+			if (s.charAt(i) == ',')
+				aux += ' ';
+			else if (Character.isDigit(s.charAt(i)))
+				aux += s.charAt(i);
+		}
+		aux = aux.trim();
+		String[] auxres = aux.split(" ");
+		// 0 -> longitude 1-> latitude
+		float res[] = { Float.valueOf(auxres[0]), Float.valueOf(auxres[1]) };
+		return res;
+	}
+
 	public static void main(String args[]) throws Exception {
-		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("AllStops.txt"))));
-		getAllStops();
-		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("AllLines.txt"))));
-		getAllLines();
+		 System.setOut(new PrintStream(new BufferedOutputStream(new
+		 FileOutputStream("AllStops.txt"))));
+		 getAllStops();
+		 System.setOut(new PrintStream(new BufferedOutputStream(new
+		 FileOutputStream("AllLines.txt"))));
+		 getAllLines();
 	}// end main
 }// end class
--- src/bus/Stop.java
+++ src/bus/Stop.java
@@ -6,15 +6,19 @@ public class Stop {
 	String address;
 	String zone;
 	String name;
+	float longitude;
+	float latitude;
 
 	Stop(String code, String address, String zone, String name) {
 		this.code = code;
 		this.address = address;
 		this.zone = zone;
 		this.name = name;
+		this.longitude = 0;
+		this.latitude = 0;
 	}
 	
 	void printStop(){
-		System.out.println("code: "+this.code+" address: "+this.address+" zone: "+this.zone+" name: "+this.name);
+		System.out.println("code: "+this.code+" address: "+this.address+" zone: "+this.zone+" name: "+this.name+" longitude: "+this.longitude+" latitude: "+this.latitude);
 	}
 }
