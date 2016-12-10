15b2612c6c18ef763ed5315ed6b152b5bd415f39
Yisgramiir <dbogas738@gmail.com>
2016-12-10 18:58:21 +0000

    oops, forgot something

--- src/bus/BusLine.java
+++ src/bus/BusLine.java
@@ -21,27 +21,5 @@ class BusLine {
 		this.LineStops = new LinkedList<String>();
 	}
 	
-	public void testPrint() throws FileNotFoundException, UnsupportedEncodingException{
-		/*File linesOutputJS = new File("/home/diogo/workspace/iic/webcrawler/lines.js");
-		PrintWriter writer = new PrintWriter("lines.js", "UTF-8");*/
-		System.out.println(this.accessibility+" "+this.code+" "+this.description+" "+this.pubcode);
 	
-	}
-	
-	public String toJS(int direction) throws Exception{
-		// output
-		
-		String res = 	"var "+this.pubcode+"_"+direction+" ={\n"
-						+"\t accessibility: "+this.accessibility+",\n"
-						+"\t pubcode: \""+this.pubcode+"\",\n"
-						+"\t code: \""+this.code+"\",\n"
-						+"\t description: \""+this.description+"\",\n"
-						+"\t paragens : [";
-		for(String s: this.LineStops){
-			if(this.LineStops.indexOf(s) == this.LineStops.size()-1) res+= "\""+s+"\"";
-			else res+= "\""+s+"\""+",";
-		}
-		res+="]\n}";
-		return res;
-	}
 }
